87f7254d0259e899de14ba5e3fa6c9529d66da85
ximonmaina <simonmaina721@gmail.com>
2019-05-28 21:39:28 +0300

    initial project structure set up

---
+++ src/main/java/sia/tacocloud/HomeController.java
@@ -0,0 +1,13 @@
+package sia.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") // handles requests for the root path
+    public String home() {
+        return "home";
+    }
+}
